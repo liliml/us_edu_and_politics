6e64ea8bc747476a3f73c6ddfe4e973bf37f1f49
liliml <lilianml@uw.edu>
2025-03-07 23:20:40 -0800

    working on nav bar style

--- js/summary_map.js
+++ js/summary_map.js
@@ -1,3 +1,26 @@
+//for nav  bar 
+// Get the element with the class "icon"
+let icon = document.getElementsByClassName("icon")[0];
+
+// Add an event listener for the 'click' event on the icon element
+icon.addEventListener('click', responsive_control);
+
+// Function to control the responsiveness of the navigation bar
+function responsive_control() {
+  // Get the element with the id "myTopnav"
+  let x = document.getElementById("myTopnav");
+
+  // Check if the class name of the element is "topnav"
+  if (x.className === "topnav") {
+    // If it is, add the "responsive" class to the element
+    x.className += " responsive";
+  } else {
+    // If it's not, remove the "responsive" class from the element
+    x.className = "topnav";
+  }
+}
+
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9zZS0xNjgiLCJhIjoiY202aWYxY3lsMDdxdjJpcHJoaHlmZzdiNiJ9.3wUanYJCI6409InuRs9e7A';
 
 // Close the welcome panel when the button is clicked
